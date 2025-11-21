3b79f447f2996d1c6fb23a019804d47b485d2c41
Collareda <131903539+Collaredaa@users.noreply.github.com>
2025-11-21 15:52:23 -0300

    Add files via upload

---
+++ bluzon/src/main/java/com/bluzon/bluzon/BluzonApplication.java
@@ -0,0 +1,13 @@
+package com.bluzon.bluzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluzonApplication.class, args);
+	}
+
+}
---
+++ bluzon/src/test/java/com/bluzon/bluzon/BluzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluzon.bluzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
